4f1c957272d10cd413b0de655c4b4fb8d28b33cd
DEVRAJ <193325790+imdevraj069@users.noreply.github.com>
2025-12-07 18:54:32 +0530

    ..a

--- node/src/server.js
+++ node/src/server.js
@@ -5,12 +5,13 @@ const app = express();
 const PORT = process.env.PORT || 8001;
 
 app.get('/', (req, res) => {
+  res.set('Cache-Control', 'public, max-age=60 s-maxage=60');
   res.send('Hello, World!');
 });
 
 app.get('/ping/python', async (req, res) => {
   try {
-    const response = await axios.get('http://python:8002/ping');
+    const response = await axios.get('http://python_app:8002/ping');
     res.json({ message: 'Pinged Python server successfully', data: response.data });
   } catch (error) {
     res.status(500).json({ message: 'Error pinging Python server', error: error.message });
