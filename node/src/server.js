e53d24d19abf759890825af3a74593536d5f7676
DEVRAJ <193325790+imdevraj069@users.noreply.github.com>
2025-12-08 03:22:46 +0530

    refactor: restructure application with MongoDB integration and improved error handling

--- node/src/server.js
+++ node/src/server.js
@@ -1,23 +1,13 @@
-import express from 'express';
-import axios from 'axios';
+import app from "./app.js";
+import { connectDB } from "./configs/connectDB.js";
+const PORT = process.env.PORT || 3000;
 
-const app = express();
-const PORT = process.env.PORT || 8001;
-
-app.get('/', (req, res) => {
-  res.set('Cache-Control', 'public, max-age=60 s-maxage=60');
-  res.send('Hello, World!');
-});
-
-app.get('/ping/python', async (req, res) => {
-  try {
-    const response = await axios.get('http://python_app:8002/ping');
-    res.json({ message: 'Pinged Python server successfully', data: response.data });
-  } catch (error) {
-    res.status(500).json({ message: 'Error pinging Python server', error: error.message });
-  }
-});
-
-app.listen(PORT, () => {
-  console.log(`Server is running on http://localhost:${PORT}`);
-});
+connectDB()
+    .then(() => {
+        app.listen(PORT, () => {
+            console.log(`Server is running on port ${PORT}`);
+        });
+    })
+    .catch((error) => {
+        console.error('Failed to start server due to database connection error:', error);
+    });
